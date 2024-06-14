0db1a64b7a5da98d78abd0bea6579eebaae1222b
Ravali <ravali.gangineni@slu.edu>
2024-06-14 00:07:49 -0500

    registration mail completed

--- src/registrationmail.js
+++ src/registrationmail.js
@@ -11,3 +11,23 @@ let transporter = nodemailer.createTransport({
     },
 });
 
+const sendRegistrationEmail = (email, name) => {
+    const mailOptions = {
+        from: `"Unity-Web" <reddyvenkatasatyasivanagendrak@gmail.com>`, // sender address
+        to: email, // list of receivers
+        subject: 'Welcome to Our App', // Subject line
+        text: `Hi ${name}, Thank You for your details , we will review and update`, // plain text body
+        html: `<b>Hello ${name},</b><br>Thank you for providing details we will let You know` // html body
+    };
+    console.log('Sending email...');
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.error('Failed to send email:', error);
+            return;
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Implement further actions if necessary, like logging or additional notifications
+    });
+}
+
+module.exports = { sendRegistrationEmail };
